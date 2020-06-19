a57152e7fb6f3f6f76d3a7a0007799ad7f8fd1f4
Ahmed Nader <ahmednader111@gmail.com>
2020-06-19 17:06:32 +0200

    Added the python socket server

---
+++ server/server.py
@@ -0,0 +1,14 @@
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+print(host)
+s.listen(5)                 # Now wait for client connection.
+while True :
+    c, addr = s.accept()     # Establish connection with client.
+    print (addr)
+    msg = "Thank you for your connection"
+    c.send(msg.encode('utf-8'))
+    c.close()                # Close the connection
