6cbf9816906bb123d8f58f7cef55effb602a1545
Ahmed Nader <ahmednader111@gmail.com>
2020-06-19 17:29:14 +0200

    Added multiple paths with multiple flights

--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 import socket               # Import socket module
 
-s = socket.socket()         # Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
